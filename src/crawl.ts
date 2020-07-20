6db361b3dd5ae9da5fc00032c004459aabc066b6
jinsunee <jsunee.dev@gmail.com>
2020-07-20 22:35:28 +0900

    add - url 입력받는 post 추가

--- src/crawl.ts
+++ src/crawl.ts
@@ -2,9 +2,9 @@ import axios, {AxiosResponse} from 'axios';
 
 import { Send } from 'express';
 
-export const crawl = (): Promise<string> => {
+export const crawl = (url: string): Promise<string> => {
   return new Promise<string>((resolve, reject) => {
-    axios.get('http://black-up.kr/product/detail.html?product_no=12786&cate_no=1&display_group=4')
+    axios.get(url)
       .then((res: AxiosResponse<any>) => {
         resolve(res.data);
         console.log(res);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// import bodyParser from 'body-parser';
 import { crawl } from './crawl';
 import express from 'express';
 import { extract } from './extract';
@@ -5,17 +6,38 @@ import { extract } from './extract';
 const app = express();
 const port = 5000;
 
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+app.use(express.json());
+// form 처리하는 것
+app.use(express.urlencoded({ extended: false }));
+
+
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send("start");
 });
 
-app.get('/crawl', async (
+// app.get('/crawl', async (
+//   req: express.Request, 
+//   res: express.Response
+// ) => {
+//   const result = await crawl();
+//   res.send(extract(result).join(',<br/>'));
+//   res.send(result);
+// });
+
+app.post('/crawl', async (  
   req: express.Request, 
   res: express.Response
-  ) => {
-  const result = await crawl();
-  res.send(extract(result).join(',<br/>'));
-  // res.send(result);
+) => {
+  const { url } = req.body;
+  
+  if (url) {
+    const result = await crawl(url);
+    console.log(extract(result).join(',<br/>'));
+    res.send(extract(result).join(',<br/>'));
+  }
 });
 
 app.listen(port, (): void => {
